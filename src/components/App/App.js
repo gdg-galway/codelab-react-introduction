ab78c1984d307a12ea365ba3ca2ada22c3c295fd
Rossano-DAngelo <rossano.dangelo@ibm.com>
2019-07-16 14:39:49 +0100

    highlighted lifecycle methods and added http requests examples

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import axios from 'axios';
+
 import "./App.css";
 
 import Counter from "../Counter/Counter";
@@ -18,7 +20,10 @@ const initialState = {
     },
     { id: Date.now() + 2, description: "git push", completed: false },
     { id: Date.now() + 3, description: "Leave the building", completed: true }
-  ]
+  ],
+  externalTodos: [],
+  posts: [],
+  loading: false
 };
 
 class App extends React.Component {
@@ -29,6 +34,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = initialState;
+    console.log('[App.js] - constructor');
   }
 
   handleToggle = id => {
@@ -43,6 +49,7 @@ class App extends React.Component {
       todos
     });
   };
+
   handleSubmit = event => {
     // Basic form handling
     event.preventDefault(); // To prevent the page reloading
@@ -59,6 +66,7 @@ class App extends React.Component {
     });
     event.target["todo-description"].value = ""; // Reset the input field
   };
+
   handleDelete = id => {
     // Another pattern to update the state
     this.setState(state => {
@@ -68,7 +76,86 @@ class App extends React.Component {
       };
     });
   };
+
+  componentDidMount = () => {
+    // This method is called as soon as the component is mounted and ready.
+    // This is a good place to initiate API calls, if you need to load data from a remote endpoint.
+    // You can update the components state within componentDidMound
+    console.log('[App.js] - componentDidMount()');
+
+    // Let's say we want to load more TODOs from an existing API
+    // and we want to use this data
+    // We need to store these external TODOs into the state
+    this.setState({ loading: true });
+    fetch('https://jsonplaceholder.typicode.com/todos')
+      .then(response => response.json())
+      .then(json => {
+        console.log('[App.js] - JSON from the API', json);
+        this.setState({ externalTodos: json });
+        console.log('[App.js] - The application state', this.state);
+        this.setState({ loading: false });
+      });
+
+    // Now we want to load the number of all posts we have in out blog
+    // But this time we'll use 'axios'
+    this.setState({ loading: true });
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then(json => {
+        // Here we can access the response object
+        // config
+        console.log('[App.js] - config', json.config);
+        // headers
+        console.log('[App.js] - headers', json.headers);
+        // request
+        console.log('[App.js] - request', json.request);
+        // status
+        console.log('[App.js] - status', json.status);
+        // and data as well
+        console.log('[App.js] - data', json.data);
+
+        this.setState({ posts: json.data });
+        this.setState({ loading: false });
+      })
+      .catch(error => {
+        // And we can easily handle errors like 404 Not Found or 500 Internal Server Error
+        console.log('[App.js] - Something went wrong!', error);
+      });
+
+      // Far more powerful than fetch, isn't? :)
+  }
+
+  componentDidUpdate = (prevProps) => {
+    // The most common use case for the componentDidUpdate() method is
+    // updating the DOM in response to prop or state changes.
+    // You can update the components state within componentDidMound
+    console.log('[App.js] - componentDidMount()');
+  }
+
+  componentWillUnmount = (prevProps) => {
+    // This methodis called just before the component is unmounted and destroyed.
+    // If there are any cleanup actions that you would need to do, this would be the right spot.
+    // You cannot update the components state within componentDidMound
+    console.log('[App.js] - componentDidMount()');
+  }
+
+  shouldComponentUpdate = (nextProps, nextState) => {
+    // This lifecycle can be handy sometimes when you don’t want React to
+    // render your state or prop changes.
+    // This method exists only for certain performance optimizations.
+    console.log('[App.js] - shouldComponentUpdate()');
+    // Returns true only if something actually change
+    console.log('[App.js] - nextProps', nextProps);
+    console.log('[App.js] - nextState', nextState);
+    return nextProps !== nextState;
+  }
+
   render() {
+    // render() is a pure function
+    // It means that this function does not have any side effects and
+    // will always return the same output when the same inputs are passed
+    // You cannot modify the component state within this method
+    console.log('[App.js] - render()');
+
     // Remember: JSX isn't HTML
     // You're using React APIs like React.createElement(...);
     // Decomment these lines and open your browser console
@@ -117,6 +204,18 @@ class App extends React.Component {
           </form>
           <Counter total={this.state.todos.length} />
         </div>
+        { this.state.loading ? 
+          <p>Loading external TODOs...</p> :
+          <div>External TODOs: <Counter total={this.state.externalTodos.length} /></div>
+        }
+        <br />
+        {this.state.loading ? (
+          <p>Loading posts...</p>
+        ) : (
+          <div>
+            Posts: <Counter total={this.state.posts.length} />
+          </div>
+        )}
         <div className="Footer">
           <code>
             with
--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,9 +1,51 @@
 import React from "react";
 
-const counter = props => (
-  <div>
-    <p>Total: {props.total}</p>
-  </div>
-);
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log('[Counter.js] - constructor');
+  }
 
-export default counter;
+  componentDidMount = () => {
+    // This method is called as soon as the component is mounted and ready.
+    // This is a good place to initiate API calls, if you need to load data from a remote endpoint.
+    // You can update the components state within componentDidMound
+    console.log('[Counter.js] - componentDidMount()');
+  }
+
+  componentDidUpdate = (prevProps) => {
+    // The most common use case for the componentDidUpdate() method is
+    // updating the DOM in response to prop or state changes.
+    // You can update the components state within componentDidMound
+    console.log('[Counter.js] - componentDidMount()');
+  }
+
+  componentWillUnmount = (prevProps) => {
+    // This methodis called just before the component is unmounted and destroyed.
+    // If there are any cleanup actions that you would need to do, this would be the right spot.
+    // You cannot update the components state within componentDidMound
+    console.log('[Counter.js] - componentDidMount()');
+  }
+
+  shouldComponentUpdate = (nextProps, nextState) => {
+    // This lifecycle can be handy sometimes when you don’t want React to
+    // render your state or prop changes.
+    // This method exists only for certain performance optimizations.
+    console.log('[Counter.js] - shouldComponentUpdate()');
+    // Returns true only if something actually change
+    console.log('[Counter.js] - nextProps', nextProps);
+    console.log('[Counter.js] - nextState', nextState);
+    return nextProps !== nextState;
+  }
+  
+  render() {
+    console.log('[Counter.js] - render()');
+    return (
+      <div>
+        <p>Total: {this.props.total}</p>
+      </div>
+    );
+  }
+}
+
+export default Counter;
